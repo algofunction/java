83c5a93e6a9da8483174563b5f0a3eaa14b05cef
Function <42390852+algofunction@users.noreply.github.com>
2024-03-30 00:56:40 +0100

    Update ex93.java

--- funda/4. Compléments d'API/ex93.java
+++ funda/4. Compléments d'API/ex93.java
@@ -1,4 +1,21 @@
-/**/
+/*Instructions
 
+Modifier la fonction min afin qu'elle puisse s'appliquer à n'importe
+quelle collection de Integer.
 
-public static
+Illustration :
+Collection<Integer> l = /* ... */ ; // [3,8,7,-1,5,8] 
+min(l) // -1 
+  **/
+
+// Solution
+public static Integer min(Collection<Integer> l) {
+  if (l.isEmpty()) throw new NoSuchElementException(); // liste vide
+  Iterator<Integer> it = l.iterator(); // création d'un itérateur
+  Integer min = it.next(); // premier élément de la liste
+  while (it.hasNext()) { // parcours du reste de la liste
+    Integer n = it.next();
+    if (n<min) min = n; // mise à jour de min
+  }
+  return min;
+}
