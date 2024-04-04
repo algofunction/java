ccd1746ade9ad5e19c6a41bcd23937497e95092b
Function <42390852+algofunction@users.noreply.github.com>
2024-04-04 19:19:39 +0200

    Update 4. Compléments d'API

---
+++ funda/4. Compléments d'API/ex99.java
@@ -0,0 +1,7 @@
+/*Instructions
+
+La fonction suivante affiche sur la sortie standard les éléments d'un objet de type ArrayList<Integer> fourni en paramètre :*/
+
+public static void affiche 
+rayList<Integer>
+
