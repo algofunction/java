2781aaa6e61cf5e1b38c54c22f6db4a659cc433c
Function <42390852+algofunction@users.noreply.github.com>
2024-04-05 23:30:42 +0200

    Update ex99.java

--- funda/4. Compléments d'API/ex99.java
+++ funda/4. Compléments d'API/ex99.java
@@ -9,5 +9,32 @@ public static void affiche(ArrayList<Integer> l) {
 
 /*Modifier la fonction afin qu'elle puisse s'appliquer à n'importe quelle collection d'éléments quelconques.
 */
+// Solution
+// Fonction renvoyant un élément de l (l est non vide) :
+public static <T> T extract(Collection<T> l) {
+  return l.iterator().next(); // résultat de type T
+}
 
-Collection<String> c1 = Arrays.asList("")
+// Fonction renvoyant un entier de l (l est non vide) :
+public static <T extends Number> int extract(Collection<T> l) {
+  return l.iterator().next().intValue(); // méthode de Number
+}
+
+// Au final, on retient les 4 variantes suivantes :
+public static <T> void affiche(Collection<T> l) {
+  for (Iterator<T> it = l.iterator(); it.hasNext(); )
+    System.out.println(it.next());
+}
+
+public static <T> void affiche(Collection<T> l) {
+  for (T t : l) System.out.println(t);
+}
+
+public static void affiche(Collection<?> l) {
+  for (Iterator<?> it = l.iterator(); it.hasNext(); )
+    System.out.println(it.next());
+}
+
+public static void affiche(Collection<?> l) {
+  for (Object o : l) System.out.println(o);
+}
