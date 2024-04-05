3833fc0a15ff8a21abd0f05e29773285a4afaa04
Function <42390852+algofunction@users.noreply.github.com>
2024-04-05 23:46:58 +0200

    Update ex100.java

--- funda/4. Compléments d'API/ex100.java
+++ funda/4. Compléments d'API/ex100.java
@@ -1,1 +1,23 @@
-/**/
+/*
+Instruction 
+
+Utiliser dans la fonction affiche la méthode forEach de Collection à la place de la
+boucle de parcours.
+Pour rappel, la méthode forEach applique une lambda expression de type cible Consumer aux éléments
+de la collection :
+@FunctionalInterface
+public interface Consumer<T> {
+  void accept(T t);
+}
+*/
+
+// Solution
+public static <T> void affiche(Collection<T> l) {
+  l.forEach( t -> System.out.println(t) );
+}
+
+public static void affiche(Collection<?> l) {
+  l.forEach( o -> System.out.println(o) );
+}
+
+
