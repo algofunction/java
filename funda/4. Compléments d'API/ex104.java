6b453436b8784d042c1094171109bbcf5f4d462e
Function <42390852+algofunction@users.noreply.github.com>
2024-04-09 21:17:20 +0200

    Create ex104.java

---
+++ funda/4. Compléments d'API/ex104.java
@@ -0,0 +1,8 @@
+/*Instructions
+
+Adapter la fonction dico afin qu'elle regroupe les mots fournis par taille et non plus
+par initiale. Illustration : 
+*/
+Collection<String> c = /*...*/
+
+// Solution
