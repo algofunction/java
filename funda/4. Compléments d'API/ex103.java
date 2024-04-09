dc660b562e9daae0212c4f1556b51a8351da49d7
Function <42390852+algofunction@users.noreply.github.com>
2024-04-09 02:14:10 +0200

    Update ex103.java

--- funda/4. Compléments d'API/ex103.java
+++ funda/4. Compléments d'API/ex103.java
@@ -1,5 +1,23 @@
 /*Instructions
+Définir la fonction dico s'appliquant à une collection de chaînes de caractères et renvoyant en résultat
+un tableau associant tous les mots fournis (valeurs) à leurs initiales (clés).
 
+Illustration :
+Collection<String> c = /* ... */;
+dico(c)
 */
 
 // Solution
+static Map<Character, Set<String>> dico(Collection<String> mots) {
+  Map<Character, Set<String>> dico = new HashMap<>();
+  for (String mot : mots) {
+    char initiale = mot.charAt(0);
+    Set<String> entree = dico.get(initiale);
+    if (entree == null) {
+      entree = new HashSet<>();
+      dico.put(initiale, entree);
+    }
+    entree.add(mot);
+  }
+  return dico;
+}
